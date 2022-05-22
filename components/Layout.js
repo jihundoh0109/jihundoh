247b310a2aaefe43260021d0cd15c4c3c8b18ce6
jamesdoh0109 <magicjcdp@gmail.com>
2022-05-21 20:16:05 -0400

    got rid of circles and added multiple background colors

--- components/Layout.js
+++ components/Layout.js
@@ -2,17 +2,13 @@ import React from 'react';
 import Navigation from './Navigation';
 import Footer from './Footer';
 import classes from './Layout.module.css';
-import Circle from './Circle';
 
 function Layout({ children }) {
   return (
-    <div style={{ position: 'relative' }}>
-      {/* <Circle /> */}
-      <div className={classes.body}>
-        <Navigation />
-        <main>{children}</main>
-        <Footer />
-      </div>
+    <div className={classes.body}>
+      <Navigation />
+      <main>{children}</main>
+      <Footer />
     </div>
   );
 }
