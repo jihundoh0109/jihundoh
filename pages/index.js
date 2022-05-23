c7f37710935839efb24c2dab681e15b6791f4675
jamesdoh0109 <magicjcdp@gmail.com>
2022-05-23 09:41:31 -0400

    bug with vertical scrolling on mobile fixed

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,11 @@ function Main() {
     <div>
       <Head>
         <title>Jihun Doh | Home</title>
+        <meta name="keywords" content="CS, James Doh, Jihun Doh, Web Dev, Full Stack, Frontend, Backend, Developer, React, NextJS" />
+        {/* todo */}
+        <meta name="description" content="" />
+        <meta name="author" content="Jihun Doh" />
+
       </Head>
       <Home />
     </div>
