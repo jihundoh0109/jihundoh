a941d87722ea8c9566abf5665467649937809549
jamesdoh0109 <magicjcdp@gmail.com>
2022-06-04 00:06:57 -0400

    project cards lined up horizontally

--- components/ProjectCards.js
+++ components/ProjectCards.js
@@ -7,8 +7,8 @@ function ProjectCards() {
   const projectListThreeAtATime = Array.from({ length: Math.ceil(projects.length / 3) }, (v, i) =>
     projects.slice(i * 3, i * 3 + 3));
 
-  const panel = (data, index) => (
-    <li>
+  const panel = (data, index, className) => (
+    <li className={classes[className]}>
       <ProjectCard
         title={data[index].title}
         img={data[index].img}
@@ -21,9 +21,9 @@ function ProjectCards() {
   return (
     <div className={classes.container} id="container">
       {projectListThreeAtATime.map((data) =>
-        data.length === 3 ? <ul>{panel(data, 0)}{panel(data, 1)}{panel(data, 2)}</ul>
-          : data.length === 2 ? <ul>{panel(data, 0)}{panel(data, 1)}</ul>
-            : data.length === 1 ? <ul>{panel(data, 0)}</ul>
+        data.length === 3 ? <ul>{panel(data, 0, 'end-3-l')}{panel(data, 1, 'middle')}{panel(data, 2, 'end-3-r')}</ul>
+          : data.length === 2 ? <ul>{panel(data, 0, 'end-2-l')}{panel(data, 1, 'end-2-r')}</ul>
+            : data.length === 1 ? <ul>{panel(data, 0, 'end-1')}</ul>
               : null)}
     </div>
   );
